d03a15cae6f278559708777c3fa6b1bf3a893ad9
banzatto <banzatto@gmail.com>
2024-04-04 22:32:31 -0300

    Adiciondo parte de conexão com banco

---
+++ src/shelter/schemas/shelter.schema.ts
@@ -0,0 +1,23 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
+import { HydratedDocument } from "mongoose"
+
+export type ShelterDocument = HydratedDocument<Shelter>
+
+@Schema({ versionKey: false })
+export class Shelter {
+    @Prop({ require: true})
+    name: string
+    @Prop({ require: true})
+    whatsApp: string
+    @Prop({ require: true})
+    email: string
+    @Prop({ require: true})
+    phone: string
+    @Prop({ require: true})
+    createdAt: Date
+    @Prop({ require: true})
+    updatedAt: Date
+
+}
+
+export const ShelterSchema = SchemaFactory.createForClass(Shelter)
